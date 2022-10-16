89e0a2680ca871622d8d4256ad5a6ad2773e3349
Graham Ollis <plicease@cpan.org>
2022-10-16 12:10:09 -0600

    add bundled Rust crate

--- lib/Memcached/RateLimit.pm
+++ lib/Memcached/RateLimit.pm
@@ -5,7 +5,13 @@ use experimental qw( postderef signatures );
 package Memcached::RateLimit {
 
   # ABSTRACT: Sliding window rate limiting with Memcached
+
+  use FFI::Platypus 2.00;
+
+  my $ffi = FFI::Platypus->new( api => 2, lang => 'Rust' );
+  $ffi->bundle;
+  $ffi->mangler(sub ($name) { "rl_$name" });
+
 }
 
 1;
-
