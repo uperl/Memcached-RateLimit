472b7d2317a991d6039f6fbb455898d131f6d8d9
Graham Ollis <plicease@cpan.org>
2022-10-17 07:01:59 -0600

    so.  um?

--- ffi/src/lib.rs
+++ ffi/src/lib.rs
@@ -2,29 +2,40 @@ use std::cell::Cell;
 use std::cell::RefCell;
 use std::collections::HashMap;
 use std::ffi::CStr;
+use std::time::SystemTime;
 use memcache::Url;
 use memcache::Client;
-use memcache::ConnectionManager;
-use r2d2::ManageConnection;
 use anyhow::Result;
 
 struct Rl {
-  manager: ConnectionManager,
+  url: Url,
   client: Option<Client>,
 }
 
 impl Rl {
   fn new(url: &CStr) -> Result<Rl> {
     let url = Url::parse(url.to_str()?)?;
-    let manager = ConnectionManager::new(url);
     Ok(Rl {
-      manager: manager,
+      url: url,
       client: None,
     })
   }
 
-  fn rate_limit(&self, prefix: &CStr, size: u64, rate_max: u64, rate_seconds: u64) -> Result<bool> {
-    Ok(false)
+  fn rate_limit(&mut self, prefix: &CStr, size: u32, rate_max: u32, rate_seconds: u32) -> Result<bool> {
+
+    /* re-connect if necessary */
+    if let None = self.client {
+      self.client = Some(Client::connect(self.url.clone().into_string())?)
+    };
+
+    if let Some(client) = &self.client {
+      let now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH)?.as_secs();
+      client.add(prefix.to_str()?, 0, rate_seconds + 1)?;
+      Ok(true)
+    } else {
+      // shouldn't get in here?
+      Ok(false)
+    }
   }
 }
 
@@ -53,6 +64,23 @@ pub extern "C" fn rl_new(url: *const i8) -> u64 {
   });
 
   index
+}
+
+#[no_mangle]
+pub extern "C" fn rl_rate_limit(index: u64, prefix: *const i8, size: u32, rate_max: u32, rate_seconds: u32) -> bool {
+  let prefix = unsafe { CStr::from_ptr(prefix) };
+
+  STORE.with(|it| {
+    let mut it = it.borrow_mut(); // does this need to be mut?
+
+    return match it.get_mut(&index) {
+      Some(rl)  => match rl.rate_limit(prefix, size, rate_max, rate_seconds) {
+        Ok(b)  => b,
+        Err(_) => {rl.client = None; false },
+      },
+      None      => false
+    }
+  })
 }
 
 #[no_mangle]
